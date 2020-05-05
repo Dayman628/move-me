e92bb903553961c6c4a38e2e0a7196e134a3edcb
Scott Harris <scottsharris@yahoo.com>
2020-05-05 11:16:47 -0700

    changed searchYelp function to reformat results and push to appropriate array

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,4 @@
+var movers = []
 var currentLocation = {
     longitude: 0,
     latitude: 0
@@ -9,11 +10,14 @@ function getLocation() {
         navigator.geolocation.getCurrentPosition(function (position) {
             currentLocation.longitude = position.coords.longitude;
             currentLocation.latitude = position.coords.latitude;
+            searchYelp('movers', movers);
+            console.log(movers);
         });
     }
 }
 
-function searchYelp(search) {
+// Get results on a yelp search
+function searchYelp(search, arr) {
     var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" + search +"&latitude=" + currentLocation.latitude + "&longitude=" + currentLocation.longitude;
     $.ajax({
         url: queryURL,
@@ -23,8 +27,18 @@ function searchYelp(search) {
         method: "GET",
         dataType: "json"
     }).then(function (response) {
-        console.log(response);
+        // Reformats and stores business results to make them easy to access
+        for (var i = 0; i < response.businesses.length; i++) {
+            var object = {};
+            object.name = response.businesses[i].name;
+            object.phone = response.businesses[i].phone;
+            object.coords = response.businesses[i].coordinates;
+            object.rating = response.businesses[i].rating;
+            arr.push(object);
+        }
     })
 }
 
 getLocation();
+
+
