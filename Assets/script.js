ea648578ef7ce51816e98065ef2ae999b5901328
Scott Harris <scottsharris@yahoo.com>
2020-05-05 10:50:19 -0700

    Added function to obtain current location & function to search yelp

---
+++ Assets/script.js
@@ -0,0 +1,30 @@
+var currentLocation = {
+    longitude: 0,
+    latitude: 0
+};
+
+// Obtains device location coordinates
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            currentLocation.longitude = position.coords.longitude;
+            currentLocation.latitude = position.coords.latitude;
+        });
+    }
+}
+
+function searchYelp(search) {
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" + search +"&latitude=" + currentLocation.latitude + "&longitude=" + currentLocation.longitude;
+    $.ajax({
+        url: queryURL,
+        headers: {
+            'Authorization': 'Bearer D_nH9CvolaBKegnkzTPM_ixwyav9S9h385FhF7zV7_F9qwlwNIjgmIhZgOQmPhA9aXfgpNL4x2136v-lGNHXGYmJYvR0yK7ZHutt_hT2FqkBK3IydmRWrPtuIOCwXnYx'
+        },
+        method: "GET",
+        dataType: "json"
+    }).then(function (response) {
+        console.log(response);
+    })
+}
+
+getLocation();
