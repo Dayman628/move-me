a7e9bdc3c38e8487118df91e0768b009b2c7a154
Linnea <linneagear@gmail.com>
2020-05-05 20:21:59 -0700

    Added comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,6 @@
 // on click, removeClass("hidden") for the second section, and addClass("hidden") to first page section and the background image
+// get APIs working
+// add improvements and functionalities once basic MVP works
 
 var movers = []
 var currentLocation = {
