5836428617609e919d38746aec0e6406f3982631
einobaka <61556425+einobaka@users.noreply.github.com>
2020-05-06 20:25:08 -0700

    moved map script into googlemap.js

--- Assets/googlemap.js
+++ Assets/googlemap.js
@@ -1,9 +1,37 @@
 // Using Google Maps API to autocomplete the search bar
-function initSearch() {
-    var input = document.getElementById('search-bar');
-    new google.maps.places.Autocomplete(input);
+// function initSearch() {
+//     var input = document.getElementById('search-bar');
+//     new google.maps.places.Autocomplete(input);
+// }
+// google.maps.event.addDomListener(window, 'load', initSearch);
+
+var currentLocation = {
+    lat: 33.448376,
+    lng: -112.074036
+};
+
+// Obtains device location coordinates
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            currentLocation.lng = position.coords.longitude;
+            currentLocation.lat = position.coords.latitude;
+        });
+    }
 }
-google.maps.event.addDomListener(window, 'load', initSearch);
+
+// code that initializes the google map
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: currentLocation,
+        zoom: 12
+    });
+}
+// Add code for when user selects "use my current location"
+// getLocation();
+
+// When address search bar is added - Google API can autocomplete addresses and convert them to coordinates and update the currentLocation variable
 
 // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see the error "The Geolocation service
