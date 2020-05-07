3f02818806f2c9bf4a1994536b7d5d03c9ec6893
Scott Harris <scottsharris@yahoo.com>
2020-05-06 19:16:00 -0700

    created yelp.js and moved searchYelp function to it

--- Assets/script.js
+++ Assets/script.js
@@ -18,28 +18,7 @@ function getLocation() {
     }
 }
 
-// Get results on a yelp search (search term, array to push results to)
-function searchYelp(search, arr) {
-    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" + search + "&latitude=" + currentLocation.latitude + "&longitude=" + currentLocation.longitude;
-    $.ajax({
-        url: queryURL,
-        headers: {
-            'Authorization': 'Bearer D_nH9CvolaBKegnkzTPM_ixwyav9S9h385FhF7zV7_F9qwlwNIjgmIhZgOQmPhA9aXfgpNL4x2136v-lGNHXGYmJYvR0yK7ZHutt_hT2FqkBK3IydmRWrPtuIOCwXnYx'
-        },
-        method: "GET",
-        dataType: "json"
-    }).then(function (response) {
-        // Reformats and stores business results to make them easy to access
-        for (var i = 0; i < response.businesses.length; i++) {
-            var object = {};
-            object.name = response.businesses[i].name;
-            object.phone = response.businesses[i].phone;
-            object.coords = response.businesses[i].coordinates;
-            object.rating = response.businesses[i].rating;
-            arr.push(object);
-        }
-    })
-}
+
 
 // code that initializes the google map
 var map;
